2c34a8989d09bffca4960ce9f90fe6c0498a83bd
WillardBowles <relic.dex@gmail.com>
2023-03-12 21:02:47 -0700

    Assignment007 Willard Bowles

--- src/MyMath.java
+++ src/MyMath.java
@@ -2,6 +2,33 @@ import java.util.Scanner;
 
 public class MyMath {
 
+    public static int gcf(int a, int b) {
+        // A is greater than B
+        if (b > a) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // Euclid's algorithm
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
     public static void main(String[] args) {
+        // Get input for a and b
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = input.nextInt();
+        System.out.print("Enter b: ");
+        int b = input.nextInt();
+
+        //Print GFC
+        System.out.println("The GCF of " + a + " and " + b + " is " + gcf(a, b));
     }
 }
